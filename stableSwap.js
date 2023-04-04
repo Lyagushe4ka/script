cd044f4bf65adb1713e796f11844752b692fd773
Lyagushka <rayz.sir@gmail.com>
2023-04-05 03:07:50 +0400

    V5

--- stableSwap.js
+++ stableSwap.js
@@ -8,76 +8,22 @@ const web3 = new Web3(new Web3.providers.HttpProvider(url));
 
 const assets = [
     {
-        name: 'WETH',
-        address: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
-        decimals: 18,
-        minBalance: 0.001
-    },
-    {
-        name: 'WMATIC',
-        address: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
-        decimals: 18,
-        minBalance: 0.1
-    },
-    {
-        name: 'WBTC',
-        address: '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6',
-        decimals: 8,
-        minBalance: 0.0001
-    },
-    {
         name: 'USDT',
         address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
         decimals: 6,
-        minBalance: 0.1
+        minBalance: 5
     },
     {
         name: 'DAI',
         address: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
         decimals: 18,
-        minBalance: 0.1
-    },
-    {
-        name: 'BAL',
-        address: '0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3',
-        decimals: 18,
-        minBalance: 0.1
+        minBalance: 5
     },
     {
         name: 'USDC',
         address: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
         decimals: 6,
-        minBalance: 0.1
-    },
-    {
-        name: 'LINK',
-        address: '0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39',
-        decimals: 18,
-        minBalance: 0.1
-    },
-    {
-        name: 'AAVE',
-        address: '0xD6DF932A45C0f255f85145f286eA0b292B21C90B',
-        decimals: 18,
-        minBalance: 0.01
-    },
-    {
-        name: 'CRV',
-        address: '0x172370d5Cd63279eFa6d502DAB29171933a610AF',
-        decimals: 18,
-        minBalance: 0.1
-    },
-    {
-        name: 'MKR',
-        address: '0x6f7C932e7684666C9fd1d44527765433e01fF61d',
-        decimals: 18,
-        minBalance: 0.001
-    },
-    {
-        name: 'SAND',
-        address: '0xBbba073C31bF03b8ACf7c28EF0738DeCF3695683',
-        decimals: 18,
-        minBalance: 0.1
+        minBalance: 5
     }
 ]
 
@@ -129,7 +75,6 @@ const types = {
     ]
 }
 
-let randAmount;
 let randTokens = [];
 let privateKeys = [];
 let randPrivateKey;
@@ -146,9 +91,9 @@ function parsePrivateKeys() {
 async function randomizeTokens(wallet) {
     let randTokenFrom = 0;
     let randTokenTo = 0;
-    randTokenTo = Math.floor(Math.random() * assets.length); // rand token between 13 options
+    randTokenTo = Math.floor(Math.random() * assets.length); // rand token between 3 options
     while (true) {
-        randTokenFrom = Math.floor(Math.random() * assets.length); // rand token between 13 options
+        randTokenFrom = Math.floor(Math.random() * assets.length); // rand token between 3 options
 
         if (randTokenFrom === randTokenTo) {
             continue;
@@ -476,23 +421,19 @@ async function main() {
 
     
     const tokenBalance = await tokenInstance(assets[randTokens[0]].address).methods.balanceOf(account.address).call() / (10 ** assets[randTokens[0]].decimals); // tokenFrom balance
-    const swapMin = assets[randTokens[0]].minBalance;
     console.log("Token 'from' is: " +  assets[randTokens[0]].name);
     console.log("Token 'to' is: " +  assets[randTokens[1]].name);
     console.log("Token 'from' balance is: " + tokenBalance);
-
-    // Randomoze token amout to swap from minAmount to token balance on account
-    randAmount = Math.random() * (tokenBalance - swapMin) + swapMin; // random amount of tokens to swap between maxNumber and minNumber
-    console.log("Amount to swap is: " + randAmount);
+    console.log("Amount to swap is: " + Math.floor(tokenBalance));
 
     // Check if the token has enough allowance
-    await isEnoughAllowance(randAmount, assets[randTokens[0]].address, account.address);
+    await isEnoughAllowance(Math.floor(tokenBalance), assets[randTokens[0]].address, account.address);
 
     const quote = await axios.get('https://api.bebop.xyz/polygon/v1/quote', {
         params: {
             buy_tokens: assets[randTokens[1]].name,
             sell_tokens: assets[randTokens[0]].name,
-            sell_amounts: randAmount.toString(),
+            sell_amounts: Math.floor(tokenBalance).toString(),
             taker_address: wallet.address.toString()
         }
     });
@@ -509,7 +450,7 @@ async function main() {
     console.log(order.data);
    
     // Random interval up to 10 minutes
-    setTimeout(main, timeoutTime = 600_000);
+    setTimeout(main, timeoutTime = Math.floor(Math.random() * (300_000 - 120_000) + 120_000));
     console.log("Timeout time is set to: " + Math.floor(timeoutTime / 60000) + ' minutes.');
 }
 main();
