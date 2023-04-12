2a1873227ae55c69f9895fc4c54a24a4df19615c
Lyagushka <rayz.sir@gmail.com>
2023-04-12 16:31:00 +0400

    Utility functions moved to functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const Web3 = require('web3');
+const colors = require('colors/safe');
+const ethers = require('ethers');
+const { SocksProxyAgent } = require('socks-proxy-agent');
+const { assets, domain, types, erc20Abi } = require('./objects');
 
 const url = "https://polygon.llamarpc.com";
 const web3 = new Web3(new Web3.providers.HttpProvider(url));
@@ -61,6 +65,130 @@ function parseData() {
     }
 }
 
+//function to take a random private key and make a wallet instance out of it
+function randomozeWallet() {
+    let randPrivateKey
+    while (true) {
+        randPrivateKey = Math.floor(Math.random() * accObjects.length)
+
+        if(accObjects[randPrivateKey].hasFinished == true) {
+            continue;
+        } else {
+            break;
+        }
+    }
+
+    const account = web3.eth.accounts.privateKeyToAccount(accObjects[randPrivateKey].key);
+    web3.eth.accounts.wallet.add(account);
+    web3.eth.defaultAccount = account.address;
+
+    const wallet = new ethers.Wallet(accObjects[randPrivateKey].key);
+
+    const proxy = new SocksProxyAgent('socks://' + accObjects[randPrivateKey].proxy)
+
+    console.log("Chosen wallet is: " + colors.cyan(account.address));
+    console.log("Choses proxy is: " + colors.cyan(accObjects[randPrivateKey].proxy));
+    return  {
+        account,
+        wallet,
+        proxy
+    }
+}
+
+// function to take 2 random tokens out of a list
+async function randomizeTokens(wallet) {
+    let randTokenFrom = 0;
+    let randTokenTo = 0;
+    while (true) {
+        randTokenTo = Math.floor(Math.random() * assets.length); // rand token between 3 options
+        randTokenFrom = Math.floor(Math.random() * assets.length); // rand token between 3 options
+
+        if (randTokenFrom === randTokenTo) {
+            continue;
+        }
+        let balance;
+        while (true) {
+            try {
+                balance = await tokenInstance(assets[randTokenFrom].address).methods.balanceOf(wallet).call();
+            } catch (err) {
+                console.log(err.message);
+                await new Promise((resolve) => {
+                    setTimeout(resolve, 1_000)
+                });
+                continue;
+            }
+            break;
+        }
+        if (balance < (assets[randTokenFrom].minBalance * (10 ** assets[randTokenFrom].decimals))) {
+            continue;
+        } else {
+            return [randTokenFrom, randTokenTo];
+        }
+    }
+}
+
+// function to check if its enough allowance to make a trade
+// if not - make an approval to MaxUint256
+async function approveTokens(amount, tokenContract, myAddress, tokenDecimals) {
+    if (await tokenInstance(tokenContract).methods.allowance(myAddress, '0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C').call() > amount * (10 ** tokenDecimals)) {
+        return;
+    } else if (tokenContract == "0xc2132D05D31c914a87C6611C10748AEb04B58e8F" && await tokenInstance(tokenContract).methods.allowance(myAddress, '0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C').call() > 0) {
+
+        const gasPrice = await web3.eth.getGasPrice();
+
+        const txRevokeData = await tokenInstance(tokenContract).methods.approve('0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C', 0);
+
+        const txRevoke = {
+            to: tokenContract,
+            data: txRevokeData.encodeABI(),
+            gas: 150000,
+            gasPrice: gasPrice
+        }
+        
+        const txRevokeSend = await web3.eth.sendTransaction(txRevoke);
+
+        console.log(`Revoke transaction sent, hash: ${txRevokeSend.transactionHash}`);
+
+        const txData = await tokenInstance(tokenContract).methods.approve('0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C', BigInt(ethers.constants.MaxUint256));
+        
+        const tx = {
+            to: tokenContract,
+            data: txData.encodeABI(),
+            gas: 150000,
+            gasPrice: gasPrice
+        };
+
+        const txSend = await web3.eth.sendTransaction(tx); // claim signed tx sending
+
+        console.log(`Approve transaction sent, hash: ${txSend.transactionHash}`);
+        
+    } else {
+        const gasPrice = await web3.eth.getGasPrice();
+
+        const txData = await tokenInstance(tokenContract).methods.approve('0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C', BigInt(ethers.constants.MaxUint256));
+        
+        const tx = {
+            to: tokenContract,
+            data: txData.encodeABI(),
+            gas: 150000,
+            gasPrice: gasPrice
+        };
+
+        const txSend = await web3.eth.sendTransaction(tx); // claim signed tx sending
+
+        console.log(colors.rainbow(`Approve transaction sent, hash: ${txSend.transactionHash}`));
+    }
+}
+
+// function to make a token instance
+function tokenInstance(contract) {
+    return new web3.eth.Contract(erc20Abi, contract);
+}
+
 module.exports = {
-    parseData
+    parseData,
+    randomozeWallet,
+    randomizeTokens,
+    approveTokens,
+    tokenInstance
 }
--- stableSwap.js
+++ stableSwap.js
@@ -5,132 +5,12 @@ const fs = require('fs');
 const { SocksProxyAgent } = require('socks-proxy-agent');
 const colors = require('colors/safe');
 const { assets, domain, types, erc20Abi } = require('./objects');
-const { parseData } = require('./functions');
+const { parseData, randomozeWallet, randomizeTokens, approveTokens, tokenInstance } = require('./functions');
 
 const url = "https://polygon.llamarpc.com";
 const web3 = new Web3(new Web3.providers.HttpProvider(url));
 
 let accObjects;
-
-// function to take 2 random tokens out of a list
-async function randomizeTokens(wallet) {
-    let randTokenFrom = 0;
-    let randTokenTo = 0;
-    while (true) {
-        randTokenTo = Math.floor(Math.random() * assets.length); // rand token between 3 options
-        randTokenFrom = Math.floor(Math.random() * assets.length); // rand token between 3 options
-
-        if (randTokenFrom === randTokenTo) {
-            continue;
-        }
-        let balance;
-        while (true) {
-            try {
-                balance = await tokenInstance(assets[randTokenFrom].address).methods.balanceOf(wallet).call();
-            } catch (err) {
-                console.log(err.message);
-                await new Promise((resolve) => {
-                    setTimeout(resolve, 1_000)
-                });
-                continue;
-            }
-            break;а
-        }
-        if (balance < (assets[randTokenFrom].minBalance * (10 ** assets[randTokenFrom].decimals))) {
-            continue;
-        } else {
-            return [randTokenFrom, randTokenTo];
-        }
-    }
-}
-
-//function to take a random private key and make a wallet instance out of it
-function randomozeWallet() {
-    let randPrivateKey
-    while (true) {
-        randPrivateKey = Math.floor(Math.random() * accObjects.length)
-
-        if(accObjects[randPrivateKey].hasFinished == true) {
-            continue;
-        } else {
-            break;
-        }
-    }
-
-    const account = web3.eth.accounts.privateKeyToAccount(accObjects[randPrivateKey].key);
-    web3.eth.accounts.wallet.add(account);
-    web3.eth.defaultAccount = account.address;
-
-    const wallet = new ethers.Wallet(accObjects[randPrivateKey].key);
-
-    const proxy = new SocksProxyAgent('socks://' + accObjects[randPrivateKey].proxy)
-
-    console.log("Chosen wallet is: " + colors.cyan(account.address));
-    console.log("Choses proxy is: " + colors.cyan(accObjects[randPrivateKey].proxy));
-    return  {
-        account,
-        wallet,
-        proxy
-    }
-}
-
-// function to make a token instance
-function tokenInstance(contract) {
-    return new web3.eth.Contract(erc20Abi, contract);
-}
-
-// function to check if its enough allowance to make a trade
-// if not - make an approval to MaxUint256
-async function approveTokens(amount, tokenContract, myAddress, tokenDecimals) {
-    if (await tokenInstance(tokenContract).methods.allowance(myAddress, '0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C').call() > amount * (10 ** tokenDecimals)) {
-        return;
-    } else if (tokenContract == "0xc2132D05D31c914a87C6611C10748AEb04B58e8F" && await tokenInstance(tokenContract).methods.allowance(myAddress, '0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C').call() > 0) {
-
-        const gasPrice = await web3.eth.getGasPrice();
-
-        const txRevokeData = await tokenInstance(tokenContract).methods.approve('0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C', 0);
-
-        const txRevoke = {
-            to: tokenContract,
-            data: txRevokeData.encodeABI(),
-            gas: 150000,
-            gasPrice: gasPrice
-        }
-        
-        const txRevokeSend = await web3.eth.sendTransaction(txRevoke);
-
-        console.log(`Revoke transaction sent, hash: ${txRevokeSend.transactionHash}`);
-
-        const txData = await tokenInstance(tokenContract).methods.approve('0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C', BigInt(ethers.constants.MaxUint256));
-        
-        const tx = {
-            to: tokenContract,
-            data: txData.encodeABI(),
-            gas: 150000,
-            gasPrice: gasPrice
-        };
-
-        const txSend = await web3.eth.sendTransaction(tx); // claim signed tx sending
-
-        console.log(`Approve transaction sent, hash: ${txSend.transactionHash}`);
-        
-    } else {
-        const gasPrice = await web3.eth.getGasPrice();
-
-        const txData = await tokenInstance(tokenContract).methods.approve('0xBeb09beB09e95E6FEBf0d6EEb1d0D46d1013CC3C', BigInt(ethers.constants.MaxUint256));
-        
-        const tx = {
-            to: tokenContract,
-            data: txData.encodeABI(),
-            gas: 150000,
-            gasPrice: gasPrice
-        };
-
-        const txSend = await web3.eth.sendTransaction(tx); // claim signed tx sending
-
-        console.log(colors.rainbow(`Approve transaction sent, hash: ${txSend.transactionHash}`));
-    }
-}
 
 async function main() {
     // parsing private keys from json file on first use
@@ -275,3 +155,8 @@ process.on('uncaughtException', (err, origin) => {
 });
 
 main();
+
+// function to check proxy connection is stable with taking proxies from the txt file
+async function proxyChecker() {
+
+}
